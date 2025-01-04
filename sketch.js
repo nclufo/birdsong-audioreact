d03bd857460227f17a3b3856036ab1d4e136a20b
nclufo <naomi.clough13@gmail.com>
2025-01-04 14:02:43 +0000

    play birdsong audio

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+var birdsong;
+
+function preload(){
+  birdsong = loadSound("birdsong audio-react.mp3");
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+}
+
+function mousePressed(){
+  birdsong.play();
 }
